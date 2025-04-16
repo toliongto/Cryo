dc353122ec853175ec2ca4f48e128bbb77d7d129
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-16 11:28:35 +0300

    syscall: Fake uname in uprobestatsbpfload
    It was recently introduced in Android 16. Probe it.

--- kernel/sys.c
+++ kernel/sys.c
@@ -1219,6 +1219,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (is_bpf_spoof_enabled()) {
 		if (!strncmp(current->comm, "bpfloader", 9) ||
 			!strncmp(current->comm, "netbpfload", 10) ||
+			!strncmp(current->comm, "uprobestatsbpfload", 18) ||
 			!strncmp(current->comm, "netd", 4)) {
 			strcpy(tmp.release, "5.4.186");
 			pr_debug("fake uname: %s/%d release=%s\n",
